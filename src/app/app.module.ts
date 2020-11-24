1c7e7ab3351f303e1c46ea4d8f764815bac55d58
Astrid Tovar <astridtovar01@gmail.com>
2020-11-24 14:38:43 -0500

    Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './shared/header/header.component';
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PagesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
